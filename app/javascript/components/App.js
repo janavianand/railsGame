d32169b09a7184e974447f63085a931919a0030f
Janavi Anand <janavianand@yahoo.com>
2019-06-30 16:32:32 -0400

    working setup with webpack yarn babel

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,11 @@
 import React from "react"
-import PropTypes from "prop-types"
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component='Home'/>
-        <Route path='/play' component = 'play'/>
-        <Route />
-      </Switch>
-      </BrowserRouter>
+      <div>
+        <h1>Hello World</h1>
+      </div>
     );
   }
 }
