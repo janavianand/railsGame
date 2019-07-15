b84c08f7468b273d058f8be19687abee86d2f8b2
Janavi Anand <janavianand@yahoo.com>
2019-07-14 23:46:07 -0400

    docker is working

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ unset BUNDLE_BIN
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# rake db:create --trace
+# rake db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
